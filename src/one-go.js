0102cfefa1ebca6d6dc1193bdc900ecfcfae5e2e
Jason Lenoble <jason.lenoble@gmail.com>
2016-11-16 16:06:46 +0100

    Made member elements private
    Use a Symbol. Now example test Testing Option 'override' fails.
    Also factored initializing code into private method init.

--- src/one-go.js
+++ src/one-go.js
@@ -8,39 +8,37 @@ export const ogArgs = function (...args) {
   return new OG(args);
 };
 
+const _init = Symbol();
+const _elements = Symbol();
+
 const OneGo = function (Class, options = {}) {
   function makeList (Class, options) {
     let List;
+    const init = function (args) {
+      this[_elements] = args.map(arg => {
+        if (arg instanceof Class) {
+          return arg;
+        }
+
+        if (Array.isArray(arg)) {
+          return new Class(...arg);
+        }
+
+        return new Class(arg);
+      });
+    };
 
     if (options.arrayInit) {
       List = function (args) {
-        this.elements = args.map(arg => {
-          if (arg instanceof Class) {
-            return arg;
-          }
-
-          if (Array.isArray(arg)) {
-            return new Class(...arg);
-          }
-
-          return new Class(arg);
-        });
+        this[_init](args);
       };
     } else {
       List = function (...args) {
-        this.elements = args.map(arg => {
-          if (arg instanceof Class) {
-            return arg;
-          }
-
-          if (Array.isArray(arg)) {
-            return new Class(...arg);
-          }
-
-          return new Class(arg);
-        });
+        this[_init](args);
       };
     }
+
+    List.prototype[_init] = init; // Use a symbol so it won't be overridden
 
     let proto = {};
 
@@ -60,15 +58,15 @@ const OneGo = function (Class, options = {}) {
           if (args0 instanceof OG) {
             const argsi = args0.args;
 
-            if (argsi.length !== this.elements.length) {
+            if (argsi.length !== this[_elements].length) {
               throw new Error(
                 `Number of elements in list and number of args don't match`);
             }
 
-            results = this.elements.map((element, ith) => proto[prop].apply(
+            results = this[_elements].map((element, ith) => proto[prop].apply(
               element, argsi[ith]));
           } else {
-            results = this.elements.map(element => proto[prop].apply(
+            results = this[_elements].map(element => proto[prop].apply(
               element, [args0, ...args]));
           }
 
